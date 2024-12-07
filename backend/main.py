d82a24d79fb6e4b7b295f45efbc447f461073bf2
Shazmeen Abbasi <94360471+Shazmeenabbasi@users.noreply.github.com>
2024-12-07 01:25:16 +0100

    Update main.py

---
+++ backend/main.py
@@ -0,0 +1,13 @@
+root@DESKTOP-MJV4C3K:/home/shazmeen/Weather-Dashboard-Budapest# git push origin main
+Username for 'https://github.com': Shazmeenabbasi
+Password for 'https://Shazmeenabbasi@github.com':
+Enumerating objects: 9, done.
+Counting objects: 100% (9/9), done.
+Delta compression using up to 8 threads
+Compressing objects: 100% (6/6), done.
+Writing objects: 100% (7/7), 858 bytes | 214.00 KiB/s, done.
+Total 7 (delta 1), reused 0 (delta 0), pack-reused 0
+remote: Resolving deltas: 100% (1/1), done.
+To https://github.com/Shazmeenabbasi/Weather-Dashboard-Budapest.git
+   3af696e..085716e  main -> main
+root@DESKTOP-MJV4C3K:/home/shazmeen/Weather-Dashboard-Budapest#
