048ce05520e5f14615688ec41f60ad1a2e4da1f5
Shazmeen Abbasi <94360471+Shazmeenabbasi@users.noreply.github.com>
2024-12-07 18:33:48 +0100

    Update script.js

--- frontend/script.js
+++ frontend/script.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
-    fetch("http://localhost:8080/weather")
+    fetch("http://localhost:5000/weather")
         .then((response) => response.json())
         .then((data) => {
             const weatherDiv = document.getElementById("weather");
